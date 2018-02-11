801bef744f38538fcb00f55ff1d1b75a80d85b9c
NightRoadMan <33252789+NightRoadMan@users.noreply.github.com>
2018-02-11 18:55:01 +0300

    Стилизация 3 секций. + карусель

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $('.multiple-items').slick({
+        dots:true,
+        slidesToShow: 1,
+        prevArrow: '<div class="prev-button"></div>',
+        nextArrow: '<div class="next-button"></div>'
+    });
+});
